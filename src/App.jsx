d0a4e2a218bd827fde18435b2a01caea79afebae
dcAngryMan <darlandc@gmail.com>
2025-09-20 15:56:13 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React JS 2026!</h1>
+      <p>Edit <code>src/App.jsx</code> and save to test hot reload.</p>
+    </div>
+  );
+}
+
+export default App;
